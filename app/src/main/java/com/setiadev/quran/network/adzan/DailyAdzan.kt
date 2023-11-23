733f0846dad8649d2c753f50891bfe37f50321dc
setianing-budi <setianingbudi2@gmail.com>
2023-11-23 07:37:35 +0700

    Get & Display Daily Adzan Time v2

---
+++ app/src/main/java/com/setiadev/quran/network/adzan/DailyAdzan.kt
@@ -0,0 +1,14 @@
+package com.setiadev.quran.network.adzan
+
+data class DailyAdzan(
+    val date: String? = null,
+    val imsak: String? = null,
+    val isya: String? = null,
+    val dzuhur: String? = null,
+    val subuh: String? = null,
+    val dhuha: String? = null,
+    val terbit: String? = null,
+    val tanggal: String? = null,
+    val ashar: String? = null,
+    val maghrib: String? = null
+)
